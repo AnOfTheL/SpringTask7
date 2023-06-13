59429a7c9cf650762e2020143e3c643c290f5b08
Darina Fedoseeva <darina.fedoseeva@rtlabs.ru>
2023-06-13 11:54:12 +0300

    added @Transactional(readOnly = true)

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,11 +1,9 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
-import web.dao.UserDaoImpl;
 import web.model.User;
 
 import java.util.List;
@@ -21,11 +19,13 @@ public class UserServiceImpl implements UserService {
     public void saveUser(User user) { userDao.saveUser(user); }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers(){
         return userDao.getAllUsers();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUserById(long id) {
         return userDao.getUserById(id);
     }
